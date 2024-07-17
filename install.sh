9d605c878fbc3ed537ff7f9c6bc98853b719b2a5
Pouria Amini <168713477+MrAminiDev@users.noreply.github.com>
2024-07-17 17:40:41 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ pip install pytz
 
 pip install requests
 
-wget https://raw.githubusercontent.com/MrAminiNezhad/3x-ui-Sanaei-TelegramBot/main/main.py
+wget https://raw.githubusercontent.com/MrAminiDev/3x-ui-Sanaei-TelegramBot/main/main.py
 
-wget https://raw.githubusercontent.com/MrAminiNezhad/3x-ui-Sanaei-TelegramBot/main/info.json
+wget https://raw.githubusercontent.com/MrAminiDev/3x-ui-Sanaei-TelegramBot/main/info.json
